c06d3606bfb6fe8e4f90d582dd076f7f9b531f0c
s2rgi0 <tlakolu@gmail.com>
2021-08-30 09:43:36 -0600

    save and display image from database unit test and functionality

---
+++ src/test/java/com/xergio/recipeapp/service/ImageServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.xergio.recipeapp.service;
+
+import com.xergio.recipeapp.commands.RecipeCommand;
+import com.xergio.recipeapp.domian.Recipe;
+import com.xergio.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ImageServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Mock
+    RecipeService recipeService;
+
+    ImageService imageService;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        //given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+                "Spring Framework Guru".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        //when
+        imageService.saveImageFile(id, multipartFile);
+
+        //then
+        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
+
+
+
+
+}
