3170d809f13def85d7063a3faafdaf248655d184
s2rgi0 <tlakolu@gmail.com>
2021-10-22 11:55:31 -0600

    adden 404not  Found error page

--- src/main/java/com/xergio/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/xergio/recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.xergio.recipeapp.controllers;
 
 import com.xergio.recipeapp.commands.RecipeCommand;
+import com.xergio.recipeapp.exceptions.NotFoundException;
 import com.xergio.recipeapp.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -51,4 +54,18 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
+
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
+    }
+
 }
