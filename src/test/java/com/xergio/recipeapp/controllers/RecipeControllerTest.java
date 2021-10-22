17dfa3ea794317bd245aa54e819a3cb4c5cd08fa
s2rgi0 <tlakolu@gmail.com>
2021-10-22 14:08:19 -0600

    adden 400 error page Bad Request for NUmber Format Excepiion

--- src/test/java/com/xergio/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/xergio/recipeapp/controllers/RecipeControllerTest.java
@@ -67,6 +67,15 @@ class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormat() throws Exception {
+
+        //when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest()).andExpect(view().name("400error"));
+    }
+
+    @Test
     void TestGetRecipe() throws Exception {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
