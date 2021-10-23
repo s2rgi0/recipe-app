076f803696f3a9cdb5759d5ab4b85e8790a3c424
s2rgi0 <tlakolu@gmail.com>
2021-10-23 15:47:49 -0600

    TDD added Test for validating Recipe form

--- src/test/java/com/xergio/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/xergio/recipeapp/controllers/RecipeControllerTest.java
@@ -99,6 +99,24 @@ class RecipeControllerTest {
                 .andExpect(model().attributeExists("recipe"));
     }
 
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
+    }
+
     @Test
     public void testPostNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
